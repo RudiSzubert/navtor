792ef3724bf2380411406dd1e003dfae8457119d
mattadtask <mateusz.tadeja+1@gmail.com>
2025-08-26 16:16:16 +0200

    chore(routing): add basic routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { VesselsComponent } from './components/vessels/vessels';
+import { EmissionsComponent } from './components/emissions/emissions';
+import { paths } from './configs/paths';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: paths.vessels, component: VesselsComponent },
+  { path: paths.emissions, component: EmissionsComponent },
+];
